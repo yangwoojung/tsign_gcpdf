71d57563cbe625aee749161747361efad0b71b51
yjpark <yjpark@tsoft.co.kr>
2022-08-11 17:53:02 +0900

    feat: Retrofit init

---
+++ src/main/java/kr/co/tsoft/sign/util/retrofit/RetrofitServiceRegistry.java
@@ -0,0 +1,16 @@
+package kr.co.tsoft.sign.util.retrofit;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RetrofitServiceRegistry {
+
+//    @Value("${example.order.base-url}")
+//    private String baseUrl;
+
+//    @Bean
+//    public RetrofitOrderApi retrofitOrderApi() {
+//        Retrofit retrofit = RetrofitUtils.initRetrofit(baseUrl);
+//        return retrofit.create(RetrofitOrderApi.class);
+//    }
+}
