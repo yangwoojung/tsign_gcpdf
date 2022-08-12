a5f8ce10c9fcce2f873fead9749e4886c25624ff
hyemiL <hmlee@tsoft.co.kr>
2022-08-12 17:53:14 +0900

    fix: Ocr response 필드 추가

--- src/main/java/kr/co/tsoft/sign/vo/ApiResponseData.java
+++ src/main/java/kr/co/tsoft/sign/vo/ApiResponseData.java
@@ -24,6 +24,12 @@ public class ApiResponseData<T> {
     @Builder
     @ToString
     public static class Ocr {
-        private final String encodeOcrFile;
+    	private final String idType; //신분증 타입     1:주민등록증, 3:운전면허증
+    	private final String socailNo; //주민등록번호
+    	private final String name; //성명
+    	private final String issueDt; //발급일자
+    	private final String licenseNo; //면허번호(운전면허증)
+    	private final String checkDigit; //식별번호(운전면허증)
+        private final String encodeOcrFile; //OCR 처리한 이미지 파일 Base64로 encode한 문자열
     }
 }
