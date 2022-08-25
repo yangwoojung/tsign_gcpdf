a80ee5ca5289e1fac7e190c4eb32cd0856c16f2c
hyemiL <hmlee@tsoft.co.kr>
2022-08-25 18:05:19 +0900

    add: Constant REQURED_VALUE 추가

--- src/main/java/kr/co/tsoft/sign/vo/common/Constant.java
+++ src/main/java/kr/co/tsoft/sign/vo/common/Constant.java
@@ -3,5 +3,7 @@ package kr.co.tsoft.sign.vo.common;
 public class Constant {
 
     public static final String ATTACHMENT_CD_ID = "001";
+    
+    public static final String REQUIRED_VALUE = "Y";
 
 }
