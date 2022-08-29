9a167218964ea312601e529a58d3301b38461a68
hyemiL <hmlee@tsoft.co.kr>
2022-08-29 16:29:15 +0900

    feat: SessionDTOMapper 생성

---
+++ src/main/java/kr/co/tsoft/sign/vo/SessionDTOMapper.java
@@ -0,0 +1,29 @@
+package kr.co.tsoft.sign.vo;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+import kr.co.tsoft.sign.config.security.CommonUserDetails;
+import kr.co.tsoft.sign.vo.ApiResponseData.Verify;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        unmappedTargetPolicy = ReportingPolicy.WARN,
+        nullValueMapMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+
+)
+public interface SessionDTOMapper {
+
+    // target method(Source source)
+	void updateUserDetails(@MappingTarget CommonUserDetails commonUserDetails, InfoDTO info);
+	void updateUserDetails(@MappingTarget CommonUserDetails commonUserDetails, RequiredApiResponseDTO requiredApiResponse);
+	void updateUserDetails(@MappingTarget CommonUserDetails commonUserDetails, ContractAttachmentDTO contractAttachment);
+}
